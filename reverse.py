ee7e6aa2bc77f2e6155392cfbc99bc75d61cf913
Anandhu Presannan <anandhupresannan@gmail.com>
2018-10-26 11:48:09 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,9 +1,9 @@
 str = "abc def"
-spnum = str.count(' ')
+spnum = str.count(' ') #count the number of spaces
 splitlist = []
 rslist = []
-splitlist = str.split(' ',spnum)
+splitlist = str.split(' ',spnum) #split the string
 for i in range(len(splitlist)):
     rs = splitlist[i][::-1]
-    rslist.append(rs)
-print ' '.join(rslist)
+    rslist.append(rs) # reverse and to the list
+print ' '.join(rslist) # join and print the output or can save into one variables
