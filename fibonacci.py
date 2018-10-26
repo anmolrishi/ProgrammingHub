167ae5ca45a1ad6c5b672648c31eec2e0b3ad918
Nathan Fei <nathanfei123@gmail.com>
2018-10-26 00:09:00 -0400

    Update fibonacci.py
    Fix syntax error in the first line

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,7 @@
-num = int(input("How many fibonacci numbers?")
+num = int(input("How many fibonacci numbers? "))
 
 def fib(n):
-    a, b = 0, 1
+    a, b = 1, 1
     for _ in range(n):
         yield a
         a, b = b, a + b
