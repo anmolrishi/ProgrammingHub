eedb7fc1c367e0b4080046d5fb0a992cf87cf75f
Chaitanya Raj <41683865+Chaitanya-Raj@users.noreply.github.com>
2018-10-27 12:06:28 +0530

    Update Fibonacci_Sequence.cpp

--- Fibonacci_Sequence.cpp
+++ Fibonacci_Sequence.cpp
@@ -8,6 +8,7 @@ void fibonacci(int n)
   cout<<"\n\n";
   long double first=0;
   long double second=1;
+  //cout<<first<<"\t"<<second;		//for generating series
   if(n==1)
   	cout<<"\n\nTerm "<<n<<" of the Fibonacci Sequence is :\t"<<first;
   if(n==2)	
@@ -18,7 +19,7 @@ void fibonacci(int n)
 	for(int i=3;i<=n;i++)
     {
       third=first+second;
-      //cout<<"\t"<<third;
+      //cout<<"\t"<<third;		//for generating series
       first=second;
       second=third;
     }
@@ -30,7 +31,7 @@ int main()
 {
   int x;
   cout<<"\n\nThis is a program to print out the Fibonacci Sequence";
-  //cout<<"\n\nChoose the number of terms upto which the fibonacci sequence is to be generated :\t";
+  //cout<<"\n\nChoose the number of terms upto which the fibonacci sequence is to be generated :\t";  //for generating series
   cout<<"\n\nWhich term of the fibonacci sequence is to be generated :\t";
   cin>>x;
   fibonacci(x);
