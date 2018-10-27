e7f4bdd780f1b7756c8a6e1fe7a4a8a3dab4de7a
SHARIF NAWAZ <43496784+SN786@users.noreply.github.com>
2018-10-27 19:28:03 +0530

    FIBANOCII SERIES AND VARIABLES

--- ha2.cpp
+++ ha2.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-    int n, t1 = 0, t2 = 1, nextTerm = 0;
+    int n, t1 = 0, t2 = 1, nt = 0;
 
     cout << "Enter the number of terms: ";
     cin >> n;
 
-    cout << "Fibonacci Series: ";
+    cout << "FIBONACCI SERIES: ";
 
     for (int i = 1; i <= n; ++i)
     {
@@ -23,11 +23,11 @@ int main()
             cout << t2 << " ";
             continue;
         }
-        nextTerm = t1 + t2;
+        nt = t1 + t2;
         t1 = t2;
-        t2 = nextTerm;
+        t2 = nt;
         
-        cout << nextTerm << " ";
+        cout << nt << " ";
     }
     return 0;
 }
