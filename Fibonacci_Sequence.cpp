3425af3950495ad865e82c78f23600ce83516c33
Chaitanya Raj <41683865+Chaitanya-Raj@users.noreply.github.com>
2018-10-27 12:00:20 +0530

    Fibonnaci Sequence in C++

---
+++ Fibonacci_Sequence.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+void fibonacci(int n)
+{
+  cout<<"\n\n";
+  long double first=0;
+  long double second=1;
+  if(n==1)
+  	cout<<"\n\nTerm "<<n<<" of the Fibonacci Sequence is :\t"<<first;
+  if(n==2)	
+	  cout<<"\n\nTerm "<<n<<" of the Fibonacci Sequence is :\t"<<second;
+  if(n>2)
+  {  
+	long double third;
+	for(int i=3;i<=n;i++)
+    {
+      third=first+second;
+      //cout<<"\t"<<third;
+      first=second;
+      second=third;
+    }
+    cout<<"\n\nTerm "<<n<<" of the Fibonacci Sequence is :\t"<<third;
+  }
+}
+ 
+int main()
+{
+  int x;
+  cout<<"\n\nThis is a program to print out the Fibonacci Sequence";
+  //cout<<"\n\nChoose the number of terms upto which the fibonacci sequence is to be generated :\t";
+  cout<<"\n\nWhich term of the fibonacci sequence is to be generated :\t";
+  cin>>x;
+  fibonacci(x);
+  getch();
+  return 0;
+}
